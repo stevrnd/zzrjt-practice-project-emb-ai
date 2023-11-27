f0a37289b68680da3017fa14194843bc4dd568d2
stevrnd <steven_diep@hotmail.co.uk>
2023-11-27 13:40:23 -0500

    saving project

--- server.py
+++ server.py
@@ -3,9 +3,20 @@
     localhost:5000.
 '''
 # Import Flask, render_template, request from the flask pramework package : TODO
+from flask import Flask, render_template, request
 # Import the sentiment_analyzer function from the package created: TODO
+from SentimentAnalysis.sentiment_analysis import sentiment_analyzer
 
 #Initiate the flask app : TODO
+app = Flask("Sentiment Analyzer")
+
+@app.route("/")
+def render_index_page():
+    ''' This function initiates the rendering of the main application
+        page over the Flask channel
+    '''
+    #TODO
+    return render_template('index.html')
 
 @app.route("/sentimentAnalyzer")
 def sent_analyzer():
@@ -15,14 +26,13 @@ def sent_analyzer():
         score for the provided text.
     '''
     # TODO
-
-@app.route("/")
-def render_index_page():
-    ''' This function initiates the rendering of the main application
-        page over the Flask channel
-    '''
-    #TODO
+    text_to_analyze = request.args.get('textToAnalyze')
+    response = sentiment_analyzer(text_to_analyze)
+    label = response['label']
+    score = response['score']
+    return "The given text has been identified as {} with a score of {}.".format(label.split('_')[1], score)
 
 if __name__ == "__main__":
     ''' This functions executes the flask app and deploys it on localhost:5000
     '''#TODO
+    app.run(host="0.0.0.0", port=5000)
